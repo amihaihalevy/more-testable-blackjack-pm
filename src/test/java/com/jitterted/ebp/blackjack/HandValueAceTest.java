404e6fd0e014b8c65f3490c1be279511e9a4177a
Ted M. Young <tedyoung@gmail.com>
2020-08-12 14:58:07 -0700

    Refactored test class to make examples (the tests) more clear.

--- src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandValueAceTest.java
@@ -2,17 +2,15 @@ package com.jitterted.ebp.blackjack;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class HandValueAceTest {
 
+  private static final Suit DUMMY_SUIT = Suit.DIAMONDS;
+
   @Test
   public void handWithOneAceTwoCardsIsValuedAt11() throws Exception {
-    List<Card> cards = List.of(new Card(Suit.DIAMONDS, Rank.from("A")),
-                               new Card(Suit.DIAMONDS, Rank.from("5")));
-    Hand hand = new Hand(cards);
+    Hand hand = createHand("A", "5");
 
     assertThat(hand.handValueOf())
         .isEqualTo(11 + 5);
@@ -20,13 +18,19 @@ public class HandValueAceTest {
 
   @Test
   public void handWithOneAceAndOtherCardsEqualTo11IsValuedAt1() throws Exception {
-    var cards = List.of(new Card(Suit.DIAMONDS, Rank.from("A")),
-                        new Card(Suit.DIAMONDS, Rank.from("8")),
-                        new Card(Suit.DIAMONDS, Rank.from("3")));
-    Hand hand = new Hand(cards);
+    Hand hand = createHand("A", "8", "3");
 
     assertThat(hand.handValueOf())
         .isEqualTo(1 + 8 + 3);
+  }
+
+
+  private Hand createHand(String... ranks) {
+    Hand hand = new Hand();
+    for (String rank : ranks) {
+      hand.add(new Card(DUMMY_SUIT, Rank.from(rank)));
+    }
+    return hand;
   }
 
 }
