493d98cd4458602a7c4d7fb000e26c0c3fc3bc7f
Ted M. Young <tedyoung@gmail.com>
2020-08-13 15:33:39 -0700

    Added bet money functionality via TDD, with some refactoring of a guard clause.

--- src/main/java/com/jitterted/ebp/blackjack/Wallet.java
+++ src/main/java/com/jitterted/ebp/blackjack/Wallet.java
@@ -3,15 +3,34 @@ package com.jitterted.ebp.blackjack;
 public class Wallet {
   private int balance;
 
+  public Wallet(int initialAmount) {
+    balance = initialAmount;
+  }
+
+  public Wallet() {
+    balance = 0;
+  }
+
   public boolean isEmpty() {
     return balance == 0;
   }
 
-  public void addMoney(int amount) {
+  public void add(int amount) {
     balance += amount;
   }
 
   public int balance() {
     return balance;
+  }
+
+  public void bet(int amount) {
+    checkSufficientBalance(amount);
+    balance -= amount;
+  }
+
+  private void checkSufficientBalance(int amount) {
+    if (amount > balance) {
+      throw new IllegalStateException();
+    }
   }
 }
--- src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/WalletTest.java
@@ -18,7 +18,7 @@ public class WalletTest {
   public void newWalletAdd10BucksResultsInWalletNotBeingEmpty() throws Exception {
     Wallet wallet = new Wallet();
 
-    wallet.addMoney(10);
+    wallet.add(10);
 
     assertThat(wallet.isEmpty())
         .isFalse();
@@ -28,7 +28,7 @@ public class WalletTest {
   public void newWalletAdd7BucksResultsInBalanceOf7Bucks() throws Exception {
     Wallet wallet = new Wallet();
 
-    wallet.addMoney(7);
+    wallet.add(7);
 
     assertThat(wallet.balance())
         .isEqualTo(7);
@@ -37,12 +37,52 @@ public class WalletTest {
   @Test
   public void walletWith13BucksAdd9BucksResultsInBalanceOf22Bucks() throws Exception {
     Wallet wallet = new Wallet();
-    wallet.addMoney(13);
+    wallet.add(13);
 
-    wallet.addMoney(9);
+    wallet.add(9);
 
     assertThat(wallet.balance())
         .isEqualTo(13 + 9);
+  }
 
+  @Test
+  public void walletWith16BucksBetting19BucksThrowsException() throws Exception {
+    Wallet wallet = new Wallet();
+    wallet.add(13);
+
+    assertThatThrownBy(() -> {
+      wallet.bet(19);
+    })
+        .isInstanceOf(IllegalStateException.class);
+  }
+
+  @Test
+  public void walletWith15BucksBetting15BucksResultsInBalanceOf0Bucks() throws Exception {
+    Wallet wallet = new Wallet();
+    wallet.add(15);
+
+    wallet.bet(15);
+
+    assertThat(wallet.balance())
+        .isZero();
+  }
+
+  @Test
+  public void walletWith18BucksBetting12BucksResultsInBalanceOf6Bucks() throws Exception {
+    Wallet wallet = new Wallet();
+    wallet.add(18);
+
+    wallet.bet(12);
+
+    assertThat(wallet.balance())
+        .isEqualTo(18 - 12);
+  }
+
+  @Test
+  public void newWalletCanBeLoadedWith100Bucks() throws Exception {
+    Wallet wallet = new Wallet(100);
+
+    assertThat(wallet.balance())
+        .isEqualTo(100);
   }
 }
